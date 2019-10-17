66128692991ccfe5ff474f17a7ebacd07ccf6f67
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-17 12:29:21 +0100

    create initial values for form

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
 
+const initialFormValues = {
+    id: Date.now(),
+    title: '',
+    director: '',
+    metascore: '',
+}
+
 export default function UpdateForm () {
+
+    
     return (
         <Formik 
+            initialValues={initialFormValues}
+            onSubmit={onAddMovie}
             render = { props => {
                 return (
                     <Form>
                         <Field name="title" type="text" placeholder="Enter title"/>
                         <Field name="director" type="text" placeholder="Enter director(s) name"/>
                         <Field name="metascore" type="text" placeholder="Enter the metascore"/>
+                        <button type="submit">Add Movie</button>
                     </Form>
                 )
             }}
